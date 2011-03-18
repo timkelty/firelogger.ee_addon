6e0c254f90a60c7359409f81cce0028bc134d0ca
Tim Kelty <timkelty@gmail.com>
2011-03-18 11:46:26 -0400

    docs url

--- system/expressionengine/third_party/firelogger/config.php
+++ system/expressionengine/third_party/firelogger/config.php
@@ -5,10 +5,10 @@ if (! defined('FIRELOGGER_NAME'))
   define('FIRELOGGER_NAME', 'FireLogger');
   define('FIRELOGGER_VER',  '0.1.0');
   define('FIRELOGGER_DESC', 'Use the FireLogger library for PHP logging through Firebug');
-  define('FIRELOGGER_DOCS', '');
+  define('FIRELOGGER_DOCS', 'https://github.com/timkelty/firelogger.ee_addon');
 }
 
 // NSM Addon Updater
-$config['name'] = FIRELOGGER_NAME;
-$config['version'] = FIRELOGGER_VER;
-$config['nsm_addon_updater']['versions_xml'] = '';
+// $config['name'] = FIRELOGGER_NAME;
+// $config['version'] = FIRELOGGER_VER;
+// $config['nsm_addon_updater']['versions_xml'] = '';
